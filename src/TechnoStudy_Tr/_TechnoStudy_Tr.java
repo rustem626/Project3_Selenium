b1e7f960a738952a697bc0c1651cc4319460ea80
yasinn35 <kayayasin541@gmail.com>
2023-09-01 21:25:44 +0300

    First commit proje başlangıç işlemleri tamamlandı

---
+++ src/TechnoStudy_Tr/_TechnoStudy_Tr.java
@@ -0,0 +1,56 @@
+package TechnoStudy_Tr;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class _TechnoStudy_Tr extends BaseDriver {
+
+    @Test
+    public void AnasayfaKursDropdownMenuGoruntuleme_01()
+    {
+
+    }
+
+    @Test
+    public void AnasayfaCampusGiris_02 ()
+    {
+
+    }
+
+    @Test
+    public void AnasayfaKursBasvurusu_03 ()
+    {
+
+    }
+
+    @Test
+    public void AltMenuKurErisim_04 ()
+    {
+
+    }
+
+    @Test
+    public void AltMenuSosyalMedyaErisim_05 ()
+    {
+
+    }
+
+    @Test
+    public void TsLogoAnaSayfaYonlendirme_06 ()
+    {
+
+    }
+
+    @Test
+    public void KursDetayliBilgi_07 ()
+    {
+
+    }
+
+    @Test
+    public void KullanimSartlariSayfaErisim_08 ()
+    {
+
+    }
+
+}
